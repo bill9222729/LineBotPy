151011aed1759b44193789b46b5bca697e6d4e25
Fegnzi <bill9222729@gmail.com>
2020-05-23 16:00:52 +0800

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort, redirect
+from flask import Flask, request, abort, redirect, render_template
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -47,6 +47,25 @@ PAY_WEB_URL = ''
 def shutdown_session(exception=None):
     db_session.remove()
 
+@app.route('/index')
+def index():
+    #路徑不須加 templates
+    return render_template(r"test.html")
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    # 需要从request对象读取表单内容：
+    if request.form['username']=='admin' and request.form['password']=='password':
+        return '<h3>Hello, admin!</h3>'
+    return '<h3>Bad username or password.</h3>'
 
 @app.route("/liff", methods=['GET'])
 def liff():
